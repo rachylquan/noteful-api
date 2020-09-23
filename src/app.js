6bb6c6307d83f39e86f941b25f14aa4ae8e572b4
Rachyl <rachylquan@yahoo.com>
2020-09-23 02:04:05 -0700

    route

--- src/app.js
+++ src/app.js
@@ -19,6 +19,10 @@ app.use(cors());
 app.use('/api/folders', foldersRouter);
 app.use('/api/notes', notesRouter);
 
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
